d1c4836f8ad76db32b5c7358af99b25e72d7f4e2
SolemnLemon <sunbear.bonzai@gmail.com>
2020-03-04 20:06:22 -0600

    final commit

--- src/emptyjavaproject/Main.java
+++ src/emptyjavaproject/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Train train = new Train();
+   
         
       roundTrip(new Walking());
       roundTrip(new Car());
